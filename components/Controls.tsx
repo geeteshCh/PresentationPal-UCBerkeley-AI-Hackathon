a9d48d96d9f8dff0cc0efeeb933a99af992a87c9
LOGANATHANAN <anloganathan20@gmail.com>
2025-02-08 14:12:45 +0100

    Third commit

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,11 +1,14 @@
 // ./components/Controls.tsx
 "use client";
 import { useVoice, VoiceReadyState } from "@humeai/voice-react";
-import { FaMicrophone } from "react-icons/fa";
+import { FaMicrophone, FaHome  } from "react-icons/fa";
+import Link from 'next/link';
+import Messages from "./Messages";
+
 export default function Controls() {
   const { connect, disconnect, readyState } = useVoice();
   const className = "px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-
+  /*
   if (readyState === VoiceReadyState.OPEN) {
     return (
       <button
@@ -17,22 +20,51 @@ export default function Controls() {
        <FaMicrophone className="text-red-600"></FaMicrophone>
       </button>
     );
-  }
+  } */
 
   return (
-    <button
-    className={className}
-      onClick={() => {
-        connect()
-          .then(() => {
-            /* handle success */
-          })
-          .catch(() => {
-            /* handle error */
-          });
-      }}
-    >
-      <FaMicrophone></FaMicrophone>
-    </button>
+    <div className="flex min-h-screen">
+    <div className="w-1/4 bg-gray-800 text-white p-6">
+    
+    <div className="mb-6">
+      <Link href="/">
+        <button className="font-bold flex items-center space-x-2">
+          <FaHome className="text-xl" />
+          <span>Go to Home Page</span>
+        </button>
+      </Link>
+    </div>
+
+    
+      <div className="flex-grow p-6 bg-gray-100">
+      <div className="flex justify-center items-center space-x-4">
+        {readyState === VoiceReadyState.OPEN ? (
+          <button
+            className={className}
+            onClick={() => {
+              disconnect();
+            }}
+          >
+            <FaMicrophone className="text-red-600" />
+          </button>
+        ) : (
+          <button
+            className={className}
+            onClick={() => {
+              connect()
+                .then(() => {})
+                .catch(() => {});
+            }}
+          >
+            <FaMicrophone />
+          </button>
+        )}
+      </div>
+    </div>
+
+    
+    </div>
+    <Messages />
+  </div>
   );
 }
