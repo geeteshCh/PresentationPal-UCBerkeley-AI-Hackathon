3a0c017a9f4aa8ab6edcc8242e23a91f19c381de
LOGANATHANAN <anloganathan20@gmail.com>
2025-02-08 15:33:58 +0100

    Check This OUT

--- app/page.tsx
+++ app/page.tsx
@@ -123,47 +123,14 @@ const HomePage = () => {
 
         {/* info section */}
         <section className="info_section layout_padding2">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6 col-lg-3 info_col">
-                
-                <div className="info_social">
-                  <a href="">
-                    <i className="fa fa-facebook" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i className="fa fa-twitter" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i className="fa fa-linkedin" aria-hidden="true"></i>
-                  </a>
-                  <a href="">
-                    <i className="fa fa-instagram" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-              <div className="col-md-1 col-lg-3 info_col">
-                <div>
-                  <h2>Powered by Hume AI</h2>
-                </div>
-              </div>
-              
-              
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            <div className="justify-content-center align-items-center">
+              <h4>Powered by Hume AI</h4>
             </div>
           </div>
+        </div>
         </section>
-        {/* end info section */}
-
-        {/* footer section */}
-        {/* <section className="footer_section">
-          <div className="container">
-            <p>
-              &copy; <span id="displayYear"></span> All Rights Reserved By
-              <a href="https://html.design/">Free Html Templates</a>
-            </p>
-          </div>
-        </section> */}
-        {/* footer section */}
       </div>
     </>
   );
--- components/ClientComponent.tsx
+++ components/ClientComponent.tsx
@@ -17,6 +17,13 @@ export default function ClientComponent({
       auth={{ type: "accessToken", value: accessToken }}
     >
       <Controls/>
+      <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <h4>
+          Powered by Hume AI
+        </h4>
+      </div>
+    </footer>
     </VoiceProvider>
     </div>
   );
